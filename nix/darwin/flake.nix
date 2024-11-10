e8cca303425fd2ee24f83b3820b0edbe934c2776
Raul Camacho <raulcd3597@gmail.com>
2024-11-09 22:06:31 -0400

    move homeconfig to own file

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -116,38 +116,8 @@
           name = "raulcamacho";
           home = "/Users/raulcamacho/";
       };
-    };
-    homeconfig = { config, pkgs, ... }:
-    {
-    # Home Manager needs a bit of information about you and the
-    # paths it should manage.
-    # home.username = "raulcamacho";
-    # home.homeDirectory = "/Users/raulcamacho";
-
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    home.stateVersion = "24.05";
-
-    # Let Home Manager install and manage itself.
-    programs.home-manager.enable = true;
-    
-    programs.git = {
-      enable = true;
-      userName = "Raul Camacho";
-      userEmail = "raulcd3597@gmail.com";
-      ignores = [ ".DS_Store" ];
-      extraConfig = {
-	init.defaultBranch = "main";
-	push.autoSetupRemote = true;
-      };
-    };
-  };  in
+  };
+  in
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#simple
@@ -168,7 +138,7 @@
        {
 	  home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.raulcamacho = homeconfig;
+          home-manager.users.raulcamacho = import ./home.nix;
        }
       ];
     };
--- nix/darwin/home.nix
+++ nix/darwin/home.nix
@@ -1,21 +1,32 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "raulcamacho";
-  home.homeDirectory = "/Users/raulcamacho";
+    # Home Manager needs a bit of information about you and the
+    # paths it should manage.
+    # home.username = "raulcamacho";
+    # home.homeDirectory = "/Users/raulcamacho";
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "24.05";
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    home.stateVersion = "24.05";
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
+    
+    programs.git = {
+      enable = true;
+      userName = "Raul Camacho";
+      userEmail = "raulcd3597@gmail.com";
+      ignores = [ ".DS_Store" ];
+      extraConfig = {
+        init.defaultBranch = "main";
+        push.autoSetupRemote = true;
+      };
+    };
 }
